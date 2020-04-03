4fab31825d2ea4701c76d4dc77354e51c7c068e7
Jeff Adams <jeffreyadams2566@gmail.com>
2020-04-03 14:40:42 -0700

    tested get username function

--- tests/edu/saddleback/cs4b/Backend/Database/DBManagerTest.java
+++ tests/edu/saddleback/cs4b/Backend/Database/DBManagerTest.java
@@ -1,5 +1,17 @@
+package edu.saddleback.cs4b.Backend.Database;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class DBManagerTest {
-
+class DBManagerTest
+{
+    @Test
+    @DisplayName("Test if database has username")
+    void getUsername() throws SQLException {
+        assertEquals("zerohezitation", DBManager.getInstance().getUsername(1));
+    }
 }
--- tests/edu/saddleback/cs4b/Backend/Database/DatabaseConnectionTest.java
+++ tests/edu/saddleback/cs4b/Backend/Database/DatabaseConnectionTest.java
@@ -1,5 +1,11 @@
+package edu.saddleback.cs4b.Backend.Database;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class DatabaseConnectionTest {
+class DatabaseConnectionTest
+{
 
 }
