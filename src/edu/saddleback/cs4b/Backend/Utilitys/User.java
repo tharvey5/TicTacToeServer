5a626d0b4da6dbbc439949e1d043f36914a42b7e
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-04 10:22:39 -0700

    User add override methods from object

--- src/edu/saddleback/cs4b/Backend/Utilitys/User.java
+++ src/edu/saddleback/cs4b/Backend/Utilitys/User.java
@@ -1,6 +1,8 @@
 package edu.saddleback.cs4b.Backend.Utilitys;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String username;
     private String fn;
     private String ln;
@@ -10,8 +12,30 @@ public class User {
     private User(String username, String fn, String ln, String password, int id) {
         this.username = username;
         this.fn = fn;
-        this.ln = ln; 
+        this.ln = ln;
         this.password = password;
         this.id = id;
+    }
+    
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        } else if (!(obj instanceof User)) {
+            return false;
+        } else {
+            return username.equals((((User) obj).username));
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        return sb.toString();
     }
 }
