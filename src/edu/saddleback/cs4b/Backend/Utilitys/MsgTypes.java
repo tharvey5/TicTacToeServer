302c61a70fd0826947c6bd2caf1dce5a16084388
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-04 10:01:12 -0700

    enum MsgTypes defined

--- src/edu/saddleback/cs4b/Backend/Utilitys/MsgTypes.java
+++ src/edu/saddleback/cs4b/Backend/Utilitys/MsgTypes.java
@@ -1,4 +1,21 @@
 package edu.saddleback.cs4b.Backend.Utilitys;
 
 public enum MsgTypes {
+    INVALID_REG("Invalid Registration"),
+    SUCCESS_REG("Successful Registration"),
+    SIGN_IN("Sign-in"),
+    AUTHENTICATION("Authentication"),
+    DENIED("Denied"),
+    SIGN_OUT("Sign-out"),
+    DISCONNECTION("Disconnection"),
+    ACTIVE_USER_REQ("Active User Request"),
+    DEACTIVATION("Deactivation");
+    // we will need to add our Reg/Profile
+
+    private String type;
+    private MsgTypes(String type) {
+        this.type = type;
+    }
+
+    public String getType() { return type; }
 }
