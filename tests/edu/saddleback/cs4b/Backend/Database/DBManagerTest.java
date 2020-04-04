7025bdbd49a6aaf3a470f3bc2a132c6777b42b38
Isaac <socalisaac@yahoo.com>
2020-04-04 14:30:41 -0700

    Tested login function of database

--- tests/edu/saddleback/cs4b/Backend/Database/DBManagerTest.java
+++ tests/edu/saddleback/cs4b/Backend/Database/DBManagerTest.java
@@ -1,5 +1,6 @@
 package edu.saddleback.cs4b.Backend.Database;
 
+import edu.saddleback.cs4b.Backend.Objects.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +12,32 @@ class DBManagerTest
 {
     @Test
     @DisplayName("Test if database has username")
-    void getUsername() throws SQLException {
-        assertEquals("zerohezitation", DBManager.getInstance().getUsername(1));
+    void getUsername() throws SQLException
+    {
+        assertEquals("Boxhead", DBManager.getInstance().getUsername(1));
     }
+
+    @Test
+    @DisplayName("Test if database has username")
+    void addUser() throws SQLException
+    {
+        User user = new User("Boxhead", "1234", "Isaac", "Estrada");
+
+        DBManager.getInstance().addUser(user);
+
+        assertEquals("Boxhead", DBManager.getInstance().getUsername(2));
+    }
+
+    @Test
+    @DisplayName("Test if Log in will find person")
+    void testLogin() throws SQLException
+    {
+        User user;
+
+        user = DBManager.getInstance().Login("Boxhead", "1234");
+
+        assertEquals("Boxhead", user.getUsername());
+    }
+
+
 }
