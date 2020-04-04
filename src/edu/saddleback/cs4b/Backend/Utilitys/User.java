26124533c8260aa5bbd51fcb26058119355a00ce
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-04 10:14:12 -0700

    user: default ctor

--- src/edu/saddleback/cs4b/Backend/Utilitys/User.java
+++ src/edu/saddleback/cs4b/Backend/Utilitys/User.java
@@ -1,4 +1,17 @@
 package edu.saddleback.cs4b.Backend.Utilitys;
 
 public class User {
+    private String username;
+    private String fn;
+    private String ln;
+    private String password;
+    private int id;
+
+    private User(String username, String fn, String ln, String password, int id) {
+        this.username = username;
+        this.fn = fn;
+        this.ln = ln; 
+        this.password = password;
+        this.id = id;
+    }
 }
