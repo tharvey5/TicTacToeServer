836eb8c2752c5b4b92f626364a54fa53df295358
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-04 10:31:18 -0700

    user: getter and setter methods

--- src/edu/saddleback/cs4b/Backend/Utilitys/User.java
+++ src/edu/saddleback/cs4b/Backend/Utilitys/User.java
@@ -10,13 +10,28 @@ public class User implements Serializable {
     private int id;
 
     private User(String username, String fn, String ln, String password, int id) {
-        this.username = username;
-        this.fn = fn;
-        this.ln = ln;
-        this.password = password;
-        this.id = id;
+        setUsername(username);
+        setFirstName(fn);
+        setLastName(ln);
+        setPassword(password);
+        setId(id);
     }
-    
+
+    public String getUsername() { return username; }
+    public String getFirstName() { return fn; }
+    public String getLn() { return ln; }
+    // todo how should we handle this?? this could be an issue if its public
+    private String getPassword() { return password; }
+
+    // todo this probably would only be used by server??? ** should it be public **
+    private int getId() { return id; }
+
+    private void setUsername(String username) { this.username = username; }
+    private void setFirstName(String fn) { this.fn = fn; }
+    private void setLastName(String ln) { this.ln = ln; }
+    private void setPassword(String password) { this.password = password; }
+    private void setId(int id) { this.id = id; }
+
     @Override
     public int hashCode() {
         return username.hashCode();
