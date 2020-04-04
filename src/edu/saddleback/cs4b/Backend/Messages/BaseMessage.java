db06e07bd9181a7d5cce2c7da418cd4b9b98a88b
adillon3 <andrewrobertdillon@gmail.com>
2020-04-04 15:10:33 -0700

    added enum

--- src/edu/saddleback/cs4b/Backend/Messages/BaseMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/BaseMessage.java
@@ -1,7 +1,26 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
+import edu.saddleback.cs4b.Backend.MessageType;
+
 import java.io.Serializable;
+import java.util.Date;
 
-public class BaseMessage implements Serializable {
 
+public abstract class BaseMessage implements Serializable {
+    MessageType messageType;
+    Date timeStamp;
+
+    public BaseMessage(MessageType newMessageType)
+    {git messageType
+        messageType = newMessageType;
+        timeStamp = new Date();
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
 }
