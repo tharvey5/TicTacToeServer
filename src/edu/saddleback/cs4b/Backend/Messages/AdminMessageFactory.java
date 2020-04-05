49a9a59b36a476da3e4cf61cba94dd1133ccc133
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-05 15:52:18 -0700

    refactor admin message factory

--- src/edu/saddleback/cs4b/Backend/Messages/AdminMessageFactory.java
+++ src/edu/saddleback/cs4b/Backend/Messages/AdminMessageFactory.java
@@ -1,14 +1,14 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
-public class AdminMsgFactory implements AbstractMessageFactory {
+public class AdminMessageFactory implements AbstractMessageFactory {
     @Override
     public  BaseMessage createMessage(String type) {
         BaseMessage message = null;
-        if (type.equals(MsgTypes.ACTIVE_USER_REQ.toString())) {
+        if (type.equals(MsgTypes.ACTIVE_USER_REQ.getType())) {
             message = new ActiveUserMessage();
-        } else  if (type.equals(MsgTypes.DISCONNECTION.toString())) {
+        } else  if (type.equals(MsgTypes.DISCONNECTION.getType())) {
             message = new DisconnectMessage();
-        } else if (type.equals(MsgTypes.SIGN_IN.toString())) {
+        } else if (type.equals(MsgTypes.SIGN_IN.getType())) {
             message = new SignInMessage();
         }
         return message;
