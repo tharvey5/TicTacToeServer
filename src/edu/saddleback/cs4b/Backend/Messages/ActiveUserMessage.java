7ec46e3962e4dc233629f4ad7511c2899fdf957f
adillon3 <andrewrobertdillon@gmail.com>
2020-04-04 18:05:24 -0700

    Created methods for ActiveUserMessage

--- src/edu/saddleback/cs4b/Backend/Messages/ActiveUserMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/ActiveUserMessage.java
@@ -1,4 +1,37 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
+import java.util.ArrayList;
+
 public class ActiveUserMessage {
+    ArrayList<String> activeUsers;
+
+    public ActiveUserMessage()
+    {
+        activeUsers = null;
+    }
+
+    public ActiveUserMessage(ArrayList<String> newActiveUsers)
+    {
+        setActiveUsers(newActiveUsers);
+    }
+
+    public void setActiveUsers(ArrayList<String> newActiveUsers)
+    {
+        activeUsers = newActiveUsers;
+    }
+
+    public ArrayList<String> getActiveUsers()
+    {
+        return activeUsers;
+    }
+
+    public void addActiveUser(String newUser)
+    {
+        activeUsers.add(newUser);
+    }
+
+    public void removeActiveUser(String oldUser)
+    {
+        activeUsers.remove(oldUser);
+    }
 }
--- src/edu/saddleback/cs4b/Backend/Messages/DisconnectMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/DisconnectMessage.java
@@ -1,4 +1,7 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
+import java.util.ArrayList;
+
 public class DisconnectMessage {
+    ArrayList
 }
