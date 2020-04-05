e803f254f135f13e92bb0e0a715e55f9e3315ed8
adillon3 <andrewrobertdillon@gmail.com>
2020-04-04 18:15:19 -0700

    Started DisconnectMessage

--- src/edu/saddleback/cs4b/Backend/Messages/DisconnectMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/DisconnectMessage.java
@@ -3,5 +3,34 @@ package edu.saddleback.cs4b.Backend.Messages;
 import java.util.ArrayList;
 
 public class DisconnectMessage {
-    ArrayList
+    ArrayList<String> channels;
+    String userName;
+
+    public DisconnectMessage(ArrayList<String> newChannels, String newUserName)
+    {
+        setChannels(newChannels);
+        setUserName(newUserName);
+    }
+
+
+
+    public ArrayList<String> getChannels()
+    {
+        return channels;
+    }
+
+    private void setChannels(ArrayList<String> channels)
+    {
+        channels = channels;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    private void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
 }
