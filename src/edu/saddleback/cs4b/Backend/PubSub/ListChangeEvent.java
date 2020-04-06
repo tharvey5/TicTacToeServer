1c3d67cb41ffdc4cce6c4e932c62196d1f015cba
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-06 10:35:37 -0700

    just set up listChangeEvent - team needs to define

---
+++ src/edu/saddleback/cs4b/Backend/PubSub/ListChangeEvent.java
@@ -0,0 +1,17 @@
+package edu.saddleback.cs4b.Backend.PubSub;
+
+
+/**
+ * need to further define what this might look lije
+ */
+public class ListChangeEvent implements SystemEvent {
+    @Override
+    public SystemEvent getEvent() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+}
