118a0f6c771863adc92f753249d52924ac885ca2
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-06 10:22:03 -0700

    defined subject interface

--- src/edu/saddleback/cs4b/Backend/PubSub/Subject.java
+++ src/edu/saddleback/cs4b/Backend/PubSub/Subject.java
@@ -1,4 +1,7 @@
 package edu.saddleback.cs4b.Backend.PubSub;
 
 public interface Subject {
+    void addObserver(Observer o);
+    void removeObserver(Observer o);
+    void notifyObserver(SystemEvent e);
 }
