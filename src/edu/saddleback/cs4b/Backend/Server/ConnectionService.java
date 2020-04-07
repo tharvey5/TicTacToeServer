0879f08ae59e4301f9f0371d807bc56b78703dc4
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-07 10:35:22 -0700

    ctor for connection service

--- src/edu/saddleback/cs4b/Backend/Server/ConnectionService.java
+++ src/edu/saddleback/cs4b/Backend/Server/ConnectionService.java
@@ -1,7 +1,24 @@
 package edu.saddleback.cs4b.Backend.Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.List;
+
 /**
- * Simply listens for new connections and manages those connections 
+ * Simply listens for new connections and manages those connections
  */
 public class ConnectionService {
+    private volatile static ConnectionService connectionService = null;
+
+    private List<ClientCommunication> connections;
+    private ServerSocket serverSocket;
+    private final static int port = 8080;
+
+    private ConnectionService() {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
