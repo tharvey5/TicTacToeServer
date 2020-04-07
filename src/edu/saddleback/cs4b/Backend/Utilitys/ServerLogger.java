1abfd37087dac93dd53a2c0bf24509b283226079
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-07 09:16:29 -0700

    serLog: member vars and priv ctor

--- src/edu/saddleback/cs4b/Backend/Utilitys/ServerLogger.java
+++ src/edu/saddleback/cs4b/Backend/Utilitys/ServerLogger.java
@@ -4,7 +4,17 @@ import edu.saddleback.cs4b.Backend.PubSub.Observer;
 import edu.saddleback.cs4b.Backend.PubSub.Subject;
 import edu.saddleback.cs4b.Backend.PubSub.SystemEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerLogger implements Logger, Subject {
+    private static ServerLogger logger = null;
+    private List<Observer> observers;
+
+    private ServerLogger() {
+        observers = new ArrayList<>();
+    }
+
     @Override
     public void addObserver(Observer o) {
 
