596a5e3f0110b7fec011341ff51f2d92844ea4a7
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-07 10:48:36 -0700

    ClientCom: ctor and inital inst methods

--- src/edu/saddleback/cs4b/Backend/Server/ClientCommunication.java
+++ src/edu/saddleback/cs4b/Backend/Server/ClientCommunication.java
@@ -1,7 +1,29 @@
 package edu.saddleback.cs4b.Backend.Server;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Handles communication with a particular client
  */
 public class ClientCommunication {
+    private Socket socket;
+    private ObjectOutputStream os;
+    private ObjectInputStream is;
+    // probably hold the profile information?
+
+    public ClientCommunication(Socket socket) {
+        this.socket = socket;
+        initiateStreams();
+    }
+
+    private void initiateStreams() {
+        try {
+            this.is = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+            this.os = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
