605a894c9bbd6b522c4c544622321a1e5e079050
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-07 10:55:37 -0700

    ClientCom: implement run method

--- src/edu/saddleback/cs4b/Backend/Server/ClientCommunication.java
+++ src/edu/saddleback/cs4b/Backend/Server/ClientCommunication.java
@@ -1,12 +1,15 @@
 package edu.saddleback.cs4b.Backend.Server;
 
+import edu.saddleback.cs4b.Backend.Messages.BaseMessage;
+import edu.saddleback.cs4b.Backend.Messages.Packet;
+
 import java.io.*;
 import java.net.Socket;
 
 /**
  * Handles communication with a particular client
  */
-public class ClientCommunication {
+public class ClientCommunication implements Runnable {
     private Socket socket;
     private ObjectOutputStream os;
     private ObjectInputStream is;
@@ -23,6 +26,26 @@ public class ClientCommunication {
             this.os = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
             os.flush();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    // the connection will listen for incoming messages
+    public void run() {
+        boolean isRunning = false;
+        try {
+            //ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+            while (isRunning) {
+                Packet packet = (Packet)is.readObject();
+                BaseMessage message = packet.getData();
+                // handleMessage()
+            }
+        } catch (EOFException eof) {
+            isRunning = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
