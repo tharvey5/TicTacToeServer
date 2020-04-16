e713ebbbbdf763575d0b6eb92f3d95c267efdc22
adillon3 <andrewrobertdillon@gmail.com>
2020-04-15 22:22:48 -0700

    made some modifications to Board interface

--- src/edu/saddleback/cs4b/Backend/Objects/Board.java
+++ src/edu/saddleback/cs4b/Backend/Objects/Board.java
@@ -3,5 +3,8 @@ package edu.saddleback.cs4b.Backend.Objects;
 public interface Board
 {
     public void setMove(Move newMove);
-    public
+
+    //doesn't make sense to make sense to arbitrary get a move, should have a location, or a move ID or something
+    public Move getMove(Coordinate moveToGet);
+    public Move getMove(String moveID);
 }
