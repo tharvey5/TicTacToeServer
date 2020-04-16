6cbeec45287f244dfb18f10203bdce6c575cda84
adillon3 <andrewrobertdillon@gmail.com>
2020-04-15 20:10:03 -0700

    Added Game and Game History interfaces

--- src/edu/saddleback/cs4b/Backend/Objects/Game.java
+++ src/edu/saddleback/cs4b/Backend/Objects/Game.java
@@ -1,13 +1,36 @@
 package edu.saddleback.cs4b.Backend.Objects;
 
-public class Game
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface Game
 {
-    private String id;
-    private String startTime;
-    private String endTime;
-    private String player1ID;
-    private String player2ID;
-    private String startingPlayerID;
-    private String winnerPlayerID;
-    private String loserPlayerID;
+    public LocalDateTime getStartTime();
+    public void setStartTime(LocalDateTime newTime);
+
+    public String getStartPlayer();
+    public void setStartPlayer(String newStartPlayer);
+
+    public String getOtherPlayer();
+    public void setOtherPlayer (String newOtherPlayer);
+
+    public List getViewers();
+    public void setViewers
+
+    public void addViewer();
+    public void removeViewer();
+
+
+    public List getMoves();
+    public void setMoves(List newMoves);
+    public void addMove(Move newMove);
+
+    public String getWinner();
+
+    public String getGameID();
+    public void setGameID(String newGameID);
+
+    public Board getGameBoard();
+    public void setGameBoard(Board newBoard);
+
 }
