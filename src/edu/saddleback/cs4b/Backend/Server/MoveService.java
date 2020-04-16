79587c5c2390ef651cbf31a65d9d8a45402f3ed1
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-15 22:58:04 -0600

    move service init

---
+++ src/edu/saddleback/cs4b/Backend/Server/MoveService.java
@@ -0,0 +1,8 @@
+package edu.saddleback.cs4b.Backend.Server;
+
+/**
+ * This services job is to provide the database with all of the recorded moves
+ */
+public class MoveService {
+    private volatile static MoveService moveService = null; 
+}
