1b2bb900f04b19d282d094640ad7ac2d28778dd6
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-16 15:39:29 -0600

    update game logic svc with rules

--- src/edu/saddleback/cs4b/Backend/Server/GameLogicService.java
+++ src/edu/saddleback/cs4b/Backend/Server/GameLogicService.java
@@ -1,8 +1,6 @@
 package edu.saddleback.cs4b.Backend.Server;
 
-import edu.saddleback.cs4b.Backend.Objects.Board;
-import edu.saddleback.cs4b.Backend.Objects.GameRules;
-import edu.saddleback.cs4b.Backend.Objects.Move;
+import edu.saddleback.cs4b.Backend.Objects.*;
 
 /**
  * This service is responsible for managing and checking the logic of a game
@@ -11,8 +9,11 @@ import edu.saddleback.cs4b.Backend.Objects.Move;
  */
 public class GameLogicService {
     private volatile static GameLogicService logicService = null;
+    private GameRules rules;
 
-    private GameLogicService() {}
+    private GameLogicService() {
+        rules = new TTTRules();
+    }
 
     public static GameLogicService getInstance() {
         if (logicService == null) {
@@ -25,14 +26,14 @@ public class GameLogicService {
         return logicService;
     }
 
-    public boolean validMove(Board board, Move move, GameRules rules) {
+    public boolean validMove(Board board, Move move) {
         return rules.inBounds(move) && rules.acceptablePlacement(board, move);
     }
 
     /**
      * returns the user name of the winner or "" if no winner
      */
-    public String findWinner(Board board, GameRules rules) {
+    public String findWinner(Board board) {
         return rules.gameWinner(board);
     }
 }
