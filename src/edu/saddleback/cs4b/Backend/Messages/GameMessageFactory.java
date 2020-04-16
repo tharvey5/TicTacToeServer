12e3dec9130c98dac22ec12b429bc846a17febba
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-15 23:46:06 -0600

    init game message factory

---
+++ src/edu/saddleback/cs4b/Backend/Messages/GameMessageFactory.java
@@ -0,0 +1,11 @@
+package edu.saddleback.cs4b.Backend.Messages;
+
+public class GameMessageFactory implements AbstractMessageFactory {
+    @Override
+    public BaseMessage createMessage(String type) {
+        BaseMessage message = null;
+
+
+        return message;
+    }
+}
