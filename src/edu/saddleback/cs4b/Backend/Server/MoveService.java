52b0b3337d6ce69427c5e25389991f5f1b7ac890
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-15 23:00:24 -0600

    move service returns instance of itself, Singleton

--- src/edu/saddleback/cs4b/Backend/Server/MoveService.java
+++ src/edu/saddleback/cs4b/Backend/Server/MoveService.java
@@ -4,5 +4,18 @@ package edu.saddleback.cs4b.Backend.Server;
  * This services job is to provide the database with all of the recorded moves
  */
 public class MoveService {
-    private volatile static MoveService moveService = null; 
+    private volatile static MoveService moveService = null;
+
+    private MoveService() {}
+
+    public static MoveService getInstance() {
+        if (moveService == null) {
+            synchronized (MoveService.class) {
+                if (moveService == null) {
+                    moveService = new MoveService();
+                }
+            }
+        }
+        return moveService;
+    }
 }
