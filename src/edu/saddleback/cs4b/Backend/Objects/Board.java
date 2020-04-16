0fac4e2b622773bd2ac2ac7b149b59cb7e0240e5
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-16 14:51:40 -0600

    board interface now has this concept as token

--- src/edu/saddleback/cs4b/Backend/Objects/Board.java
+++ src/edu/saddleback/cs4b/Backend/Objects/Board.java
@@ -2,8 +2,8 @@ package edu.saddleback.cs4b.Backend.Objects;
 
 public interface Board
 {
-   void setToken(int r, int c);
-   void setToken(Coordinate coordinate);
+   void setToken(int r, int c, Token token);
+   void setToken(Coordinate coordinate, Token token);
    Board getBoard();
    Token getToken(int r, int c);
 }
