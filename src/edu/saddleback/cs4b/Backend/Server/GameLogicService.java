a423b14d0caad77952a91adac5682fe1c2cc90d5
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-16 00:25:39 -0600

    game logic svc will now validate move and find winner

--- src/edu/saddleback/cs4b/Backend/Server/GameLogicService.java
+++ src/edu/saddleback/cs4b/Backend/Server/GameLogicService.java
@@ -1,7 +1,12 @@
 package edu.saddleback.cs4b.Backend.Server;
 
+import edu.saddleback.cs4b.Backend.Objects.Board;
+import edu.saddleback.cs4b.Backend.Objects.Move;
+
 /**
  * This service is responsible for managing and checking the logic of a game
+ * This service will validate the move based on a rules can check if there was a
+ * winner based on rules
  */
 public class GameLogicService {
     private volatile static GameLogicService logicService = null;
@@ -17,5 +22,16 @@ public class GameLogicService {
             }
         }
         return logicService;
+    }
+
+    public boolean validMove(Move move, GameRule rules) {
+        return rules.acceptablePlacement(move);
+    }
+
+    /**
+     * returns the user name of the winner or "" if no winner
+     */
+    public String findWinner(Board board, GameRule rules) {
+        return rules.gameWinner(board);
     }
 }
