dec72f77c0672b657bb50eb5be1f7bb5ba21d019
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-15 23:53:43 -0600

    game message factory setup

--- src/edu/saddleback/cs4b/Backend/Messages/GameMessageFactory.java
+++ src/edu/saddleback/cs4b/Backend/Messages/GameMessageFactory.java
@@ -4,8 +4,33 @@ public class GameMessageFactory implements AbstractMessageFactory {
     @Override
     public BaseMessage createMessage(String type) {
         BaseMessage message = null;
-
-
+        if (type.equals(MsgTypes.VIEW_GAME_REQUEST.getType())) {
+            message = new ViewGameRequestMessage();
+        }
+        else if (type.equals(MsgTypes.GAME_HISTORY_REQUEST.getType())) {
+            message = new GameHistoryRequestMessage();
+        }
+        else if (type.equals(MsgTypes.ACTIVE_GAMES.getType())) {
+            message = new GetAllActiveGamesMessage();
+        }
+        else if (type.equals(MsgTypes.MOVE.getType())) {
+            message = new MoveMessage();
+        }
+        else if (type.equals(MsgTypes.INVALID_MOVE.getType())) {
+            message = new InvalidMoveMessage();
+        }
+        else if (type.equals(MsgTypes.GAME_RESULT.getType())) {
+            message = new GameResultMessage();
+        }
+        else if (type.equals(MsgTypes.JOIN_GAME_REQUEST.getType())) {
+            message = new JoinGameRequestMessage();
+        }
+        else if (type.equals(MsgTypes.CREATE_GAME.getType())) {
+            message = new CreateGameMessage();
+        }
+        else if (type.equals(MsgTypes.GAME_HISTORY_RESPONSE.getType())) {
+            message = new GameHistoryResponseMessage();
+        }
         return message;
     }
 }
