b99fc97dac3adb3ce7b47ad46bbd5fd4b0587a37
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-16 15:11:00 -0600

    horizontal winner set

--- src/edu/saddleback/cs4b/Backend/Objects/TTTRules.java
+++ src/edu/saddleback/cs4b/Backend/Objects/TTTRules.java
@@ -23,4 +23,15 @@ public class TTTRules implements GameRules {
         }
         return winningToken;
     }
+
+    private String horizontalWinner(Board board) {
+        for (int r = 0; r < board.rows(); ++r) {
+            if (board.getToken(r, 0).equals(board.getToken(r, 1)) &&
+                board.getToken(r, 1).equals(board.getToken(r, 2)) &&
+                board.getToken(r, 0) != null) {
+                return board.getToken(r, 0).getTokenID();
+            }
+        }
+        return null;
+    }
 }
