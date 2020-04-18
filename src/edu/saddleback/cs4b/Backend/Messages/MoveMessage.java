c9e99d639e9cefeca491565337bebc04652060c1
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-17 18:05:14 -0600

    move message

--- src/edu/saddleback/cs4b/Backend/Messages/MoveMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/MoveMessage.java
@@ -1,27 +1,29 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
+import edu.saddleback.cs4b.Backend.Objects.Coordinate;
+
 public class MoveMessage extends BaseMessage
 {
-    TTTPosition coordinate;
+    Coordinate coordinate;
 
     public MoveMessage()
     {
         this(null);
     }
 
-    public MoveMessage(TTTPosition newCoordinate)
+    public MoveMessage(Coordinate newCoordinate)
     {
         super(MsgTypes.MOVE);
 
         coordinate = newCoordinate;
     }
 
-    public TTTPosition getCoordinate()
+    public Coordinate getCoordinate()
     {
         return coordinate;
     }
 
-    public void setCoordinate(TTTPosition newCoord)
+    public void setCoordinate(Coordinate newCoord)
     {
         coordinate = newCoord;
     }
