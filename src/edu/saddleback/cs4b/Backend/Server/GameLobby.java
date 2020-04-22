3c9ce9e5973a884f887baf6ae2965c4de38a88f2
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-21 20:32:32 -0600

    lobby allows users to create a game

--- src/edu/saddleback/cs4b/Backend/Server/GameLobby.java
+++ src/edu/saddleback/cs4b/Backend/Server/GameLobby.java
@@ -1,11 +1,25 @@
 package edu.saddleback.cs4b.Backend.Server;
 
+import edu.saddleback.cs4b.Backend.Objects.Board;
+import edu.saddleback.cs4b.Backend.Objects.Game;
+import edu.saddleback.cs4b.Backend.Utilitys.PublicUser;
+
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * This will allow people to create games and view games
  * It will run on its own thread and thats
  */
 public class GameLobby {
     private volatile static GameLobby lobby = null;
+    private Map<String, Game> activeGames;  // key = gameID, value = Game
+                                            // don't return game but maybe
+                                            // return who is playing the game
+
+    private GameLobby() {
+        activeGames = new Hashtable<>();
+    }
 
     public static GameLobby getInstance() {
         if (lobby == null) {
@@ -16,5 +30,15 @@ public class GameLobby {
             }
         }
         return lobby;
+    }
+
+    public Board createGame(PublicUser user) {
+        // create the new game
+        // request the id from the DB
+        // assign the id to the game
+        // assign the user as the observer by calling the
+        // system info svc to map the user, to their system representation
+        // return the board of the created game
+        return null;
     }
 }
