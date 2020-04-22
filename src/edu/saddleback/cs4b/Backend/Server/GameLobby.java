d7d2a050c427c080550203a300f3b89c212a9f00
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-21 20:26:41 -0600

    game lobby singleton

--- src/edu/saddleback/cs4b/Backend/Server/GameLobby.java
+++ src/edu/saddleback/cs4b/Backend/Server/GameLobby.java
@@ -5,4 +5,16 @@ package edu.saddleback.cs4b.Backend.Server;
  * It will run on its own thread and thats
  */
 public class GameLobby {
+    private volatile static GameLobby lobby = null;
+
+    public static GameLobby getInstance() {
+        if (lobby == null) {
+            synchronized (GameLobby.class) {
+                if (lobby == null) {
+                    lobby = new GameLobby();
+                }
+            }
+        }
+        return lobby;
+    }
 }
