0fd9523ad36511ab97a2345940de02c8fda86190
Tucker Harvey <coopert.harvey@gmail.com>
2020-05-03 21:15:47 -0700

    updated availGameMsg

--- src/edu/saddleback/cs4b/Backend/Messages/AvailableGameMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/AvailableGameMessage.java
@@ -5,10 +5,11 @@ import edu.saddleback.cs4b.Backend.Objects.Game;
 public class AvailableGameMessage extends BaseMessage
 {
     private Game game;
+    private String gameId;
 
     public AvailableGameMessage()
     {
-        this(null);
+        this(null, null);
     }
 
     public AvailableGameMessage(Game newGame)
@@ -16,6 +17,19 @@ public class AvailableGameMessage extends BaseMessage
         super(MsgTypes.AVAILABLE_GAME);
         game = newGame;
     }
+
+    public AvailableGameMessage(Game game, String gameId) {
+        super(MsgTypes.AVAILABLE_GAME);
+        setGame(game);
+        setGameId(gameId);
+    }
+
+    public AvailableGameMessage(String gameId){
+        this(null, gameId);
+    }
+
+    public void setGameId(String gameId) { this.gameId = gameId; }
+    public String getGameId() { return gameId; }
 
     public Game getGame()
     {
