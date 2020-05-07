011198b93d026dec1c36c586951100dbeddb8b70
Tucker Harvey <coopert.harvey@gmail.com>
2020-05-06 20:40:34 -0700

    activegames displays in tableView

--- src/edu/saddleback/cs4b/UI/ActiveGamesController.java
+++ src/edu/saddleback/cs4b/UI/ActiveGamesController.java
@@ -1,23 +1,30 @@
 package edu.saddleback.cs4b.UI;
 
 import edu.saddleback.cs4b.Backend.Messages.*;
+import edu.saddleback.cs4b.Backend.Objects.Game;
 import edu.saddleback.cs4b.Backend.PubSub.MessageEvent;
 import edu.saddleback.cs4b.Backend.PubSub.Observer;
 import edu.saddleback.cs4b.Backend.PubSub.SystemEvent;
 import edu.saddleback.cs4b.Backend.Server.*;
+import edu.saddleback.cs4b.UI.Utilities.GameInfo;
 import edu.saddleback.cs4b.UI.Utilities.UILogger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Color;
 
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
-public class ActiveGamesController implements Observer
+public class ActiveGamesController implements Observer, Initializable
 {
     private Logger logger = UILogger.getInstance();
 
@@ -25,23 +32,33 @@ public class ActiveGamesController implements Observer
     private Button refreshButton;
 
     @FXML
-    private TableView activeGamesTable;
+    private TableView<GameInfo> activeGamesTable;
 
     @FXML
-    private TableColumn idCol;
+    private TableColumn<GameInfo, String> idCol;
 
     @FXML
-    private TableColumn gameCol;
+    private TableColumn<GameInfo, String> gameCol;
 
     @FXML
-    private TableColumn p1Col;
+    private TableColumn<GameInfo, String> p1Col;
 
     @FXML
-    private TableColumn p2Col;
+    private TableColumn<GameInfo, String> p2Col;
+
+    private ObservableList<GameInfo> gameInfo = FXCollections.observableArrayList();
 
 
     public ActiveGamesController() {
         ServerLogger.getInstance().addObserver(this);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        gameCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        p1Col.setCellValueFactory(new PropertyValueFactory<>("p1"));
+        p2Col.setCellValueFactory(new PropertyValueFactory<>("p2"));
     }
 
     // recieves information when ever the server logger gets log called
@@ -63,16 +80,21 @@ public class ActiveGamesController implements Observer
 
     private void displayToUI(ReturnAllActiveGamesMessage message) {
         Map<String, List<String>> map = message.getGameAndPlayers();
+        GameInfo addedInfo = null;
 
         for (Map.Entry<String,List<String>> entry : map.entrySet()) {
-           //id.setCellFactory(new PropertyValueFactory<String, List<String>>("id"));
-            System.out.println(entry.getKey());
-
+            addedInfo = new GameInfo();
+            addedInfo.setId(entry.getKey());
             // print the players
-            for (String player : map.get(entry.getKey())) {
-                System.out.println(player);
+            List<String> players = map.get(entry.getKey());
+            addedInfo.setP1(players.get(0));
+            if (players.size() == 2) {
+                addedInfo.setP2(players.get(1));
             }
+            addedInfo.setTitle(players.get(0)+ "\'s game");
+            gameInfo.add(addedInfo);
         }
+        activeGamesTable.setItems(gameInfo);
     }
 
     @FXML
